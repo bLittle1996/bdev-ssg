c8427e2141d38bf12a855ce58a7c9f54f7eacf08
Ben Littleton <github.blizzard230@passmail.net>
2025-04-27 12:52:23 -0400

    added leaf node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -24,17 +24,44 @@ class HTMLNode:
         return " ".join(map(lambda v: self.__prop_to_html(*v), self.props.items()))
 
     def __prop_to_html(self, key: str, value: str) -> str:
-        return f"{key}=\"{value.replace('"', "&quot;")}\""
+        return f'{key}="{self._escape_special_chars(value)}"'
+
+    # for text content or attribute content of a html node
+    def _escape_special_chars(self, text: str) -> str:
+        return text.replace('"', "&quot;").replace("<", "&lt;").replace(">", "&gt;")
 
     def __repr__(self) -> str:
         props = ""
         if self.tag:
             props = self.tag
         if self.value:
-            props += f", {self.value}"
+            props += f', "{self.value}"'
         if self.children:
             props += f", {len(self.children)} child nodes"
         if self.props:
             props += f", {self.props}"
 
+        if props.startswith(", "):
+            props = props.replace(", ", "")
+
         return f"HTMLNode({props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(
+        self,
+        tag: Optional[str],
+        value: str,
+        props: dict[str, str] = {},
+    ):
+        super().__init__(tag, value, [], props)
+        self.value = value  # for type system
+
+    def to_html(self) -> str:
+        cleaned_value = self._escape_special_chars(self.value)
+        if not self.tag:
+            return cleaned_value
+        if len(self.props) == 0:
+            return f"<{self.tag}>{cleaned_value}</{self.tag}>"
+
+        return f"<{self.tag} {self.props_to_html()}>{cleaned_value}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class HTMLNodeTest(unittest.TestCase):
@@ -17,3 +17,21 @@ class HTMLNodeTest(unittest.TestCase):
 
         for node, expected in cases:
             self.assertEqual(node.props_to_html(), expected, node)
+
+
+class LeafNodeTest(unittest.TestCase):
+    def test_to_html(self):
+        cases = [
+            (LeafNode(None, ""), ""),
+            (LeafNode(None, "Hello, world!"), "Hello, world!"),
+            (LeafNode("p", ""), "<p></p>"),
+            (LeafNode("p", "<div>lmao</div>"), "<p>&lt;div&gt;lmao&lt;/div&gt;</p>"),
+            (LeafNode("p", "just some normal text"), "<p>just some normal text</p>"),
+            (
+                LeafNode("p", "just some text", {"data-kind": "normal"}),
+                '<p data-kind="normal">just some text</p>',
+            ),
+        ]
+
+        for node, expected in cases:
+            self.assertEqual(node.to_html(), expected, node)

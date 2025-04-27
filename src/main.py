7cba6c35d90486f6095c0a1d59251075af25c88f
Ben Littleton <github.blizzard230@passmail.net>
2025-04-27 11:24:47 -0400

    textnode base added

---
+++ src/main.py
@@ -0,0 +1,9 @@
+
+from textnode import TextNode, TextType
+
+
+def main():
+    print(TextNode("Hello, world!", TextType.BOLD))
+
+if __name__ == "__main__":
+    main()
---
+++ src/textnode.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+class TextType(Enum):
+    TEXT = "TEXT"
+    BOLD = "BOLD"
+    ITALIC = "ITALIC"
+    CODE = "CODE"
+    LINK = "LINK"
+    IMAGE = "IMG"
+
+class TextNode:
+    def __init__(self, text: str, type: TextType, url: str | None = None ) -> None:
+        self.text = text
+        self.type = type
+        self.url = url
+
+    def __eq__(self, value: object) -> bool:
+        if not isinstance(value, type(self)):
+            return False
+
+        return self.type == value.type and self.text == value.text and self.url == value.url
+
+    def __repr__(self) -> str:
+        if self.type in (TextType.IMAGE, TextType.LINK):
+            return f"TextNode({self.type.value}, \"{self.text}\", \"{self.url}\")"
+        return f"TextNode({self.type.value}, \"{self.text}\")" 
+
